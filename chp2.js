8e0c48f3700d3904647b8da46d39e3070acdaffe
rafay123rehman <155463436+rafay123rehman@users.noreply.github.com>
2024-03-04 10:26:56 +0500

    Add files via upload

---
+++ chp2.js
@@ -0,0 +1,36 @@
+//question1
+var username = 'rafay';
+
+//question2
+var myname = "rehman";
+alert(username+myname);
+
+//question3
+var message;
+message = "hello world";
+alert(message);
+
+//question4
+alert(username+myname);
+var age = '18';
+alert(age);
+var cerfication = "enter cleard by pre engineering";
+alert(certification);
+
+//question5
+alert("pizza /n burger /n conldrink /n sandwich");
+
+//question6
+var email = "my email is "+"example@example.com";
+alert(email);
+
+//question7
+var book = "A smarter way to learn java script";
+alert(book);
+
+//question8
+document.write("yah! i can write html content through javascript");
+
+//question9
+var design ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design);
---
+++ chp4.js
@@ -0,0 +1,8 @@
+//question1
+//var name can contain only letter, numbers, dollar signs and underscores
+
+//question2 and 3
+// Variable names can only containnumbers,$, and _. For example : $my_$my_1stVariable
+// Variables must begin with a $ or _ For example : $name,_name or name
+// Variable names are case sensitive
+// Variable names should not be JS keywords
